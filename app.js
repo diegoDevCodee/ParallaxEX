44a9d6c4683b8cf5d6b2174335fe04f479056c0d
diegoDevCodee <143859659+diegoDevCodee@users.noreply.github.com>
2023-10-02 01:08:53 -0500

    Parallax

---
+++ app.js
@@ -0,0 +1,6 @@
+let scene = document.getElementById("scene");
+let text = document.getElementById("text");
+
+let parallaxInstanceScene = new Parallax(scene);
+let parallaxInstanceText = new Parallax(text);
+
